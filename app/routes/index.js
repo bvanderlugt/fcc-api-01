3dbe3482a3f27ae5c69f873b2891cc381c7a343f
Blair Vanderlugt <blair.vanderlugt@gmail.com>
2016-06-23 19:40:32 +0000

    remvoed a bunch of dumb stuff

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,68 +1,68 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+// var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 var dateBuilder = require(path + '/app/controllers/dateBuilder.server.js');
 
 // module.exports = function (app, passport) {
 module.exports = function (app) {	
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+	// function isLoggedIn (req, res, next) {
+	// 	if (req.isAuthenticated()) {
+	// 		return next();
+	// 	} else {
+	// 		res.redirect('/login');
+	// 	}
+	// }
 
-	var clickHandler = new ClickHandler();
+	// var clickHandler = new ClickHandler();
 
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
+	// app.route('/')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.sendFile(path + '/public/index.html');
+	// 	});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+	// app.route('/login')
+	// 	.get(function (req, res) {
+	// 		res.sendFile(path + '/public/login.html');
+	// 	});
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+	// app.route('/logout')
+	// 	.get(function (req, res) {
+	// 		req.logout();
+	// 		res.redirect('/login');
+	// 	});
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
+	// app.route('/profile')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.sendFile(path + '/public/profile.html');
+	// 	});
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+	// app.route('/api/:id')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.json(req.user.github);
+	// 	});
 
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
+	// app.route('/auth/github')
+	// 	.get(passport.authenticate('github'));
 
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
+	// app.route('/auth/github/callback')
+	// 	.get(passport.authenticate('github', {
+	// 		successRedirect: '/',
+	// 		failureRedirect: '/login'
+	// 	}));
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	// app.route('/api/:id/clicks')
+	// 	.get(isLoggedIn, clickHandler.getClicks)
+	// 	.post(isLoggedIn, clickHandler.addClick)
+	// 	.delete(isLoggedIn, clickHandler.resetClicks);
 	
-	app.route('/timestamp')
+	app.route('/')
 		.get(function(req, res) {
 		    res.sendFile(path + '/public/timestamp.html');
 		});
 	
-	app.route('/timestamp/:time')
+	app.route('/:time')
 		.get(function(req, res) {
 			var t = dateBuilder(req);
 			res.send(t);
